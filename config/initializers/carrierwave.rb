8a21c72c3f9a7716ce458c497633a4d1e22c6aa2
yukino0307 <62429496+yukino0307@users.noreply.github.com>
2020-05-18 01:43:47 +0900

    環境変数を設定

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,22 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_DEFAULT_REGION'],
+  }
+
+  config.fog_directory  = 'kokoiru'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kokoiru'
+  end
+
+end
