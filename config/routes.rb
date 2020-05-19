2f29d872a27e995a610dc4672d9b921bdcbff38d
yukino0307 <62429496+yukino0307@users.noreply.github.com>
2020-05-20 03:47:30 +0900

    共通部分の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
   root 'posts#index'
+
+  resources :posts do
+  end
+
+  resources :users, only: [:index, :edit, :update]
 end
