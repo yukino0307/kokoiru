96354d4fb25c9c0a33f480837cd0f9837247786c
yukino0307 <62429496+yukino0307@users.noreply.github.com>
2020-05-24 23:58:17 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
 
-  config.storage = :fog
+  config.storage = :file
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
